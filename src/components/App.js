7970530fc000a1d45e83f8be546aab567e551166
Lavruk Yulia <lavruk_yulia@ukr.net>
2019-07-11 01:13:06 +0300

    add prev, next, reset

--- src/components/App.js
+++ src/components/App.js
@@ -9,6 +9,7 @@ export default class App extends React.Component {
 		super()
 
 		this.state = {
+			step: 0,
 			firstname:"",
 			lastname:"",
 			password:"",
@@ -48,41 +49,62 @@ export default class App extends React.Component {
 		reader.readAsDataURL(event.target.files[0])
 	};
 
+	onPrev = event =>{
+		const nextStep = this.state.step - 1
+		this.setState({
+			errors: {},
+			step: nextStep
+		})
+
+		console.log("state", this.state);
+	};
+
 	onNext = event =>{
 		const errors = {};
-		if (this.state.firstname.length < 5) {
-			errors.firstname = 'Must be 5 characters or more'
+		if(this.state.step === 0){
+			if (this.state.firstname.length < 5) {
+				errors.firstname = 'Must be 5 characters or more'
+			}
+			if (this.state.lastname.length < 5) {
+				errors.lastname = 'Must be 5 characters or more'
+			}
+			if(this.state.password.length < 6){
+				errors.password = 'Must be 6 characters or more'
+			}
+			if(this.state.password !==this.state.repeatPassword){
+				errors.repeatPassword = 'Must be equal password'
+			}
 		}
-		if (this.state.lastname.length < 5) {
-			errors.lastname = 'Must be 5 characters or more'
-		}
-		if(this.state.password.length <3){
-			errors.password = 'Must be 3 characters or more'
-		}
-		if(this.state.password !==this.state.repeatPassword){
-			errors.repeatPassword = 'Must be equal password'
-		}
-		if(this.state.email.length < 5){
+		if (this.state.step === 1) {
+			if(this.state.email.length < 5){
 			errors.email = 'Must be 5 characters or more'
+			}
+			if(this.state.mobile.length < 10){
+				errors.mobile = 'Must be 10 characters or more'
+			}
+			if(this.state.city === ""){
+				errors.city = 'Required'
+			}
 		}
-		if(this.state.mobile.length < 10){
-			errors.mobile = 'Must be 10 characters or more'
+		
+		if(this.state.step === 2){
+			if(this.state.avatar === ""){
+				errors.avatar = 'Required'
+			}
 		}
-		if(this.state.city === ""){
-			errors.city = 'Required'
-		}
-		if(this.state.avatar === ""){
-			errors.avatar = 'Required'
-		}
+
 		if(Object.keys(errors).length > 0){
 			this.setState({
 				errors: errors
 			})
 		}
 		else{
+			const nextStep = this.state.step + 1
 			this.setState({
-				errors: {}
+				errors: {},
+				step: nextStep
 			})
+
 			console.log("state", this.state);
 		}
 	}
@@ -91,29 +113,63 @@ export default class App extends React.Component {
 		//console.log(this.state)
 	    return (
 	     	<div className="form-container card">
-	        <form className="form card-body">
+		        <form className="form card-body">
+			        <div className="steps d-flex justify-content-between">
+			        	<div className="step">
+			        		<div className="step-number">1</div>
+			        		<div className="step-title">Basic</div>
+			        	</div>
+			        	<div className="step">
+			        		<div className="step-number">2</div>
+			        		<div className="step-title">Contacts</div>
+			        	</div>
+			        	<div className="step">
+			        		<div className="step-number">3</div>
+			        		<div className="step-title">Avatar</div>
+			        	</div>
+			        	<div className="step">
+			        		<div className="step-number">4</div>
+			        		<div className="step-title">Finish</div>
+			        	</div>
+			        </div>
+	        {this.state.step === 0 &&(
 	        	<Basic 
 	        		value={this.state} 
 	        		onChange={this.onChange} 
 	        		error={this.state.errors}
 	        		gender={this.state.gender}
-	        	/>
+	        	/>)}
+	        {this.state.step === 1 &&(
 	        	<Contacts 
 	        		value={this.state} 
 	        		onChange={this.onChange} 
 	        		error={this.state.errors}
-	        	/>
+	        	/>)}
+	        {this.state.step === 2 &&(
 	        	<Avatar 
 	        		value={this.state} 
 	        		onChangeAvatar={this.onChangeAvatar}
 	        		error= {this.state.errors}
-	        	/>
+	        	/>)}
+	        {this.state.step === 3 &&(
 	        	<Finish 
 	        		value={this.state}
-	        	/>
+	        	/>)}
 	        	<div className="d-flex">
-	        		<button type="button" className="btn btn-secondary">Prev</button>
+	        	{this.state.step < 3 ?(
+	        		<div>
+	        		<button 
+	        			type="button" 
+	        			className="btn btn-secondary" 
+	        			onClick={this.onPrev}
+	        			disabled={this.state.step === 0}
+	        		>
+	        			Prev
+	        		</button>
 	        		<button type="button" className="btn btn-secondary" onClick={this.onNext}>Next</button>
+	        		</div>)
+	        		:
+	        		<button type="button" className="btn btn-primary" onClick={()=>{window.location.reload()}}>Reset</button>}
 	        	</div>
 	        </form>
 	      	</div>
