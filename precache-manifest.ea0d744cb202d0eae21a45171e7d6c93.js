19ff6e33fb76eb34d9e7f7ebb8f95cdeae0619d7
Lavruk Yulia <lavruk_yulia@ukr.net>
2020-03-13 13:49:27 +0200

    Updates

---
+++ precache-manifest.ea0d744cb202d0eae21a45171e7d6c93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a840803fd759abda8e2",
+    "url": "/reactwarriors-form-task/static/js/runtime~main.9c3fa9ae.js"
+  },
+  {
+    "revision": "6e1eb48c3aa19ce949aa",
+    "url": "/reactwarriors-form-task/static/js/main.c0cda4fe.chunk.js"
+  },
+  {
+    "revision": "13712c778d6a460a3459",
+    "url": "/reactwarriors-form-task/static/js/2.6834dc90.chunk.js"
+  },
+  {
+    "revision": "6e1eb48c3aa19ce949aa",
+    "url": "/reactwarriors-form-task/static/css/main.477b1008.chunk.css"
+  },
+  {
+    "revision": "13712c778d6a460a3459",
+    "url": "/reactwarriors-form-task/static/css/2.ee53c3dc.chunk.css"
+  },
+  {
+    "revision": "9d56e9b8d006ca7eae0cfcf187e65f01",
+    "url": "/reactwarriors-form-task/index.html"
+  }
+];
